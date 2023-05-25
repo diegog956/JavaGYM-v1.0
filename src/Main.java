8093c172e66d151ce83f7462c8fea6d3c1be0099
diegog956 <119092215+diegog956@users.noreply.github.com>
2023-05-25 20:19:57 -0300

    Update Main.java
    Modifiqué.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Version modificada");
+        System.out.println("Version modificada 2 veces");
     }
 }
