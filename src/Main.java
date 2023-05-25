579d2e528b5cd23c71ca3abb5bbb14965f7a3419
Diego <diego.gerardo95@hotmail.com>
2023-05-25 20:29:31 -0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Version modificada 2 veces");
+        System.out.println("Version modificada");
     }
 }
